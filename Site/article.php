1aa4f93515712e77b74b21c179cdc61ca9b0f27e
Elvis Tran <XoreandoX@hotmail.com>
2017-05-25 23:07:21 -0400

    Added Simple SQL Code for connection
    Added some basic connection SQL code for checking the DB if the
    connection exists and a generic SQL query for a specific person in a
    generic table.

--- Site/article.php
+++ Site/article.php
@@ -3,48 +3,90 @@
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $comment = $website = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed"; 
-    }
-  }
+$serverName = "localhost";
+$username = $email;
+$password = "password";
+$dbName = "myDB";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+	if (empty($_POST["name"])) 
+	{
+		$nameErr = "Name is required";
+	}
+	else
+	{
+		$name = test_input($_POST["name"]);
+		// check if name only contains letters and whitespace
+		if (!preg_match("/^[a-zA-Z ]*$/",$name)) 
+		{
+			$nameErr = "Only letters and white space allowed"; 
+		}
+	}
   
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
-    }
-  }
+	if (empty($_POST["email"])) 
+	{
+		$emailErr = "Email is required";
+	}
+	else 
+	{
+		$email = test_input($_POST["email"]);
+		// check if e-mail address is well-formed
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+		{
+		$emailErr = "Invalid email format"; 
+		}
+	}
     
-  if (empty($_POST["website"])) {
-    $website = "";
-  } else {
-    $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-      $websiteErr = "Invalid URL"; 
-    }
-  }
+	if (empty($_POST["website"])) 
+	{
+		$website = "";
+	} 
+	else 
+	{
+		$website = test_input($_POST["website"]);
+		// check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+		if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) 
+		{
+			$websiteErr = "Invalid URL"; 
+		}
+	}
 
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
-  }
+	if (empty($_POST["comment"])) 
+	{
+		$comment = "";
+	} 
+	else 
+	{
+		$comment = test_input($_POST["comment"]);
+	}
+  
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) 
+	{
+		die("Connection failed: " . $conn->connect_error);
+	} 
 
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) 
+	{
+		// output data of each row
+		while($row = $result->fetch_assoc()) 
+		{
+			echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+		}
+	} 
+	else 
+	{
+		echo "0 results";
+	}
+$conn->close();
+  
+  
 }
 
 function securityFilter()
